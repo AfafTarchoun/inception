657a4756972c2553c29607b7901f2465782e669d
AfafTarchoun <tarchounafaf@gmail.com>
2023-05-14 15:39:43 +0100

    try

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 service mysql start
-mariadb -u root -e "CREATE DATABASE $MYSQL_DATABASE ;"
-mariadb -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ; GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' ; FLUSH PRIVILEGES ;"
+mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE ;"
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ; GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' ; FLUSH PRIVILEGES ;"
 #mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_pass'; FLUSH PRIVILEGES;"
 exec $@
