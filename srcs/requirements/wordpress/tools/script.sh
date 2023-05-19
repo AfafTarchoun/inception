eb49d6df8b6dc35425451e3de72a1611b3f302a6
AfafTarchoun <tarchounafaf@gmail.com>
2023-05-19 06:05:50 +0100

    please

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
-cd /var/www/html/wordpress
+
+cd /var/www/html
+
+rm -rf *
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
+chmod +x wp-cli.phar 
+
+mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
-#touch /run/php/php7.3-fpm.sock
 
-#chown -R www-data:www-data /run/php
+mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
-#chmod 660 /run/php/php7.3-fpm.sock
+sed -i -r "s/database/$db_name/1"   wp-config.php
 
-wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
+sed -i -r "s/database_user/$db_user/1"  wp-config.php
 
-wp core install --url=${DOMAIN_NAME} --title=Inception --admin_user=${WP_AD} --admin_password=${WP_PW} --admin_email=${WP_EMAIL} --allow-root
+sed -i -r "s/passwod/$db_pwd/1"    wp-config.php
 
-wp user create ${WP_USER} --user_email=${WP_USER_EMAIL} --user_pass=${WP_USR_PW} --role=author --allow-root
+sed -i -r "s/localhost/mariadb/1"    wp-config.php  (to connect with mariadb database)
+
+# installs WordPress and sets up the basic configuration for the site. The --url option specifies the URL of the site, --title sets the site's title, --admin_user and --admin_password set the username and password for the site's administrator account, and --admin_email sets the email address for the administrator. The --skip-email flag prevents WP-CLI from sending an email to the administrator with the login details.
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+# creates a new user account with the specified username, email address, and password. The --role option sets the user's role to author, which gives the user the ability to publish and manage their own posts.
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+
+/usr/sbin/php-fpm7.3 -F
