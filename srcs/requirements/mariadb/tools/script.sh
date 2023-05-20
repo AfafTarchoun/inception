5e56b19907951a5d88d85db1213476cca9fdfd5c
AfafTarchoun <tarchounafaf@gmail.com>
2023-05-20 06:21:30 +0100

    fix wordpress installation

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,4 +2,4 @@
 service mysql start
 mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE ;"
 mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ; FLUSH PRIVILEGES ;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_pass'; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
