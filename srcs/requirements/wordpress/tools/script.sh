6170144b24b94860d701aed3cad26c0912c3b983
AfafTarchoun <tarchounafaf@gmail.com>
2023-05-20 00:07:30 -0400

    almost

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
-cd /var/www/html
+cd /var/www/html/wordpress
 
-rm -rf *
+wp core download --path="/var/www/html/wordpress" --allow-root
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-
-chmod +x wp-cli.phar 
-
-mv wp-cli.phar /usr/local/bin/wp
-
-wp core download --allow-root
-
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
-
-sed -i -r "s/database/$db_name/1"   wp-config.php
-
-sed -i -r "s/database_user/$db_user/1"  wp-config.php
-
-sed -i -r "s/passwod/$db_pwd/1"    wp-config.php
-
-sed -i -r "s/localhost/mariadb/1"    wp-config.php  (to connect with mariadb database)
-
-# installs WordPress and sets up the basic configuration for the site. The --url option specifies the URL of the site, --title sets the site's title, --admin_user and --admin_password set the username and password for the site's administrator account, and --admin_email sets the email address for the administrator. The --skip-email flag prevents WP-CLI from sending an email to the administrator with the login details.
-wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-
-# creates a new user account with the specified username, email address, and password. The --role option sets the user's role to author, which gives the user the ability to publish and manage their own posts.
-wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
-
-mkdir /run/php
-
-/usr/sbin/php-fpm7.3 -F
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --dbprefix=wp
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
+wp user create --path="/var/www/html/wordpress" --allow-root $WP_USR $WP_EMAIL --user_pass=$WP_USER_PWD
+chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
+mkdir -p /run/php
+php-fpm7.3 -F
--- srcs/srcs/requirements/mariadb/tools/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-service mysql start
-mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE ;"
-mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ; FLUSH PRIVILEGES ;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'new_pass'; FLUSH PRIVILEGES;"
