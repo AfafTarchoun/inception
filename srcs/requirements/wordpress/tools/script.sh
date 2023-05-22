80229610d69834848465b4a1dacce99d174c21e3
AfafTarchoun <tarchounafaf@gmail.com>
2023-05-22 03:38:26 -0400

    database connection error

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,19 +4,15 @@ cd /var/www/html/wordpress
 
 wp core download --path="/var/www/html/wordpress" --allow-root
 
-wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_DBHOST --dbprefix=wp --extra-php <<PHP
-define('WP_DEBUG', true);
-define('WP_DEBUG_DISPLAY', false);
-define('WP_DEBUG_LOG', true);
-PHP
+#wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_DBHOST --dbprefix=wp
 
-cp wp-config.php /var/www/html/wordpress/wp-config.php
+#cp wp-sample-config.php /var/www/html/wordpress/wp-config.php
 
 wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
 
 wp user create --path="/var/www/html/wordpress" --allow-root $WP_USR $WP_EMAIL --user_pass=$WP_USER_PWD
 
-chown www-data:www-data /var/www/html/wordpress/wp-content/uploads -R
+chown www-data:www-data /var/www/html/wordpress/ -R
 
 mkdir -p /run/php/
 
--- srcs/requirements/wordpress/tools/wp-sample-config.php
+++ srcs/requirements/wordpress/tools/wp-sample-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', '${MYSQL_USER}' );
 define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );
 
 /** Database hostname */
-define( 'DB_HOST', '${MYSQL_DBHOST}' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
