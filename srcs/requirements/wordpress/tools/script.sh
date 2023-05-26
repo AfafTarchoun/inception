5a0daaf73f3f8f0cb05b1b67f5c0cd09331e2807
AfafTarchoun <tarchounafaf@gmail.com>
2023-05-26 13:55:29 -0400

    mind blowing inception

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,9 +4,9 @@ cd /var/www/html/wordpress
 
 wp core download --path="/var/www/html/wordpress" --allow-root
 
-wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbprefix=wp
+wp config create --path="/var/www/html/wordpress" --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --dbprefix=wp_
 
-wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
+wp core install --path="/var/www/html/wordpress" --allow-root --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL
 
 wp user create --path="/var/www/html/wordpress" --allow-root $WP_USR $WP_EMAIL --user_pass=$WP_USER_PWD
 
